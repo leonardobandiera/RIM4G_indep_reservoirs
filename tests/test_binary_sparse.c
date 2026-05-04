181c8bf4d8839b5f16e74c685fb72a253ed45109
leobm <147031896+leonardobandiera@users.noreply.github.com>
2026-05-04 17:03:00 +0200

    Refactor binary sparse matrix test for symmetry

--- tests/test_binary_sparse.c
+++ tests/test_binary_sparse.c
@@ -12,29 +12,29 @@ specifically for binary sparse matrices.
 void main() {
 
         int row_dimension = 20;
-        int column_dimension = 20;
-        int n_connections = 4;
-        int n_blocks = 5;
+        int n_connections = 3;
+        int n_blocks = 4;
 
-        float **M = binary_sparse(row_dimension, column_dimension, n_connections, 42, n_blocks);
+        float **M = binary_symmetric_sparse_blocks(row_dimension, n_connections, n_blocks, random_state=42);
 		
     	// print matrix
     	printf("Matrix M:\n");
     	for(int i = 0; i < row_dimension; i++){
-        	for(int j = 0; j < column_dimension; j++){
+        	for(int j = 0; j < row_dimension; j++){
             	printf("%2.0f ", M[i][j]); 
         	}
        	    printf("\n");
     	}
     	printf("\n");
 
-        int block_size = column_dimension/n_blocks;
+        int block_size = row_dimension/n_blocks;
 	for(int i=0; i<row_dimension; i++){
 		int block_id = i/block_size;
 		int start = block_id*block_size;
 		int nnz = 0; //non zero elements, used to check the number of connections is respected
 
-		for(int j=0; j<column_dimension; j++){
+		for(int j=0; j<row_dimension; j++){
+			assert(M[i][j]==M[j][i]);
 			assert(M[i][j]==0 || M[i][j]==1 || M[i][j]==-1);
 			if(M[i][j]!=0){
 				nnz++;
